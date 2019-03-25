1d95aa789774d2bd2eaf4a5841604f6f4b6838a0
Arttu Janhunen <arttu.janhunen@gmail.com>
2019-03-25 19:09:27 +0200

    Maksukortin testit

--- laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
+++ laskarit/viikko2/Unicafe/src/test/java/com/mycompany/unicafe/MaksukorttiTest.java
@@ -15,6 +15,66 @@ public class MaksukorttiTest {
 
     @Test
     public void luotuKorttiOlemassa() {
-        assertTrue(kortti!=null);      
+        assertTrue(kortti != null);
+    }
+
+    @Test
+    public void saldoOnOikeinAlussa() {
+
+        assertEquals(10, kortti.saldo());
+    }
+
+    @Test
+    public void saldoNouseeOikeinRahaaLadatessa() {
+        kortti.lataaRahaa(3);
+
+        assertEquals(13, kortti.saldo());
+    }
+
+    @Test
+    public void saldoVaheneeOkein() {
+        kortti.otaRahaa(5);
+
+        assertEquals(5, kortti.saldo());
+    }
+
+    @Test
+    public void saldoEiMuutuJosEiTarpeeksi() {
+        kortti.otaRahaa(11);
+
+        assertEquals(10, kortti.saldo());
+    }
+
+    @Test
+    public void tililtaVoiOttaaSaldonVerran() {
+        kortti.otaRahaa(10);
+
+        assertEquals(0, kortti.saldo());
+    }
+
+    @Test
+    public void otaRahaaPalauttaaTrueJosRiittaa() {
+
+        assertEquals(true, kortti.otaRahaa(1));
+    }
+
+    @Test
+    public void otaRahaaPalauttaaFalseJosEiRiita() {
+
+        assertEquals(false, kortti.otaRahaa(11));
+    }
+
+    @Test
+    public void toStringPalauttaaOikeanTulosteen() {
+
+        assertEquals("saldo: 0.10", kortti.toString());
+    }
+    
+    @Test 
+    public void toinenToStringTesti(){
+        Maksukortti vaihtoehtoKortti= new Maksukortti(105);
+        
+        assertEquals("saldo: 1.05", vaihtoehtoKortti.toString());
+        
     }
 }
